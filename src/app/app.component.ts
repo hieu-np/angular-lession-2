5fa1e485a081e30f49b3d47ab11f4345f7126a84
nguyenhieu409 <hieu.nguyen@successsoftware.global>
2022-06-27 17:29:58 +0700

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lession-02-pipe';
+  content: string =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+  name:string='lương định của';
+  title: string = 'Học Angular 13';
+  amount: number = 35.142567;
+  result : number = 0.8516
+
 }
